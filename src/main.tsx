2f0f9cbe0f0757ab09826c68b7553eafc159de95
Diego Zamora <diegozar02@gmail.com>
2026-01-21 01:25:08 +0000

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./styles/index.scss";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
